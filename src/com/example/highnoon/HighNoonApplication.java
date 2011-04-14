477d35b1507d4c351ec5c64c813054321787894e
Marco Jahn <me@marco-jahn.info>
2011-04-14 17:48:38 +0200

    - formatting - file comments

--- src/com/example/highnoon/HighNoonApplication.java
+++ src/com/example/highnoon/HighNoonApplication.java
@@ -2,20 +2,40 @@ package com.example.highnoon;
 
 import android.app.Application;
 
-public class HighNoonApplication extends Application {
+/**
+ * HighNoon Application app singleton.
+ * 
+ * @author Marco Jahn <me@marco-jahn.info>
+ * 
+ */
+public class HighNoonApplication
+        extends Application {
 
-	private static HighNoonApplication instance;
-	
-	public HighNoonApplication() {
-		HighNoonApplication.instance = this;
-	}
-	
-	@Override
-	public void onCreate() {
-		
-	}
-	
-	public static HighNoonApplication getInstance() {
-		return instance;
-	}
+    /**
+     * instance Instanz der Applikation
+     */
+    private static HighNoonApplication instance;
+
+    /**
+     * 
+     * Constructs a HighNoonApplication.
+     * 
+     */
+    public HighNoonApplication() {
+
+        HighNoonApplication.instance = this;
+    }
+
+    public void onCreate() {
+
+    }
+
+    /**
+     * 
+     * @return Applikationsinstanz
+     */
+    public static HighNoonApplication getInstance() {
+
+        return instance;
+    }
 }
--- src/com/example/highnoon/activities/HighNoonActivity.java
+++ src/com/example/highnoon/activities/HighNoonActivity.java
@@ -1,15 +1,22 @@
 package com.example.highnoon.activities;
 
-import com.example.highnoon.R;
-import com.example.highnoon.R.layout;
-
 import android.app.Activity;
 import android.os.Bundle;
 
-public class HighNoonActivity extends Activity {
+import com.example.highnoon.R;
+
+/**
+ * HighNoon Start activity
+ * 
+ * @author Marco Jahn <me@marco-jahn.info>
+ * 
+ */
+public class HighNoonActivity
+        extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
